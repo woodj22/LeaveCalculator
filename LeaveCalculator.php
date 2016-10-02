1a58595af416b38ef7ffeaee5f94a5a922a982cd
JoeWood <JoeWood@JoeWoods-MacBook-Pro.local>
2016-10-02 18:38:45 +0100

    initiaition of first file of LeaveCalculator.

---
+++ LeaveCalculator.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JoeWood
+ * Date: 02/10/2016
+ * Time: 18:36
+ */
+
+
+class LeaveCalculator {
+
+
+public function start(){
+    echo "hello world";
+
+
+}
+
+
+}
+
+
+
+$Lc = new LeaveCalculator();
