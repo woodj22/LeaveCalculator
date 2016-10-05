d985c215f271e7ce69c94c608b2a0c8e6fbebe83
Joe Wood <BBCWood@bbcwood.national.core.bbc.co.uk>
2016-10-05 17:03:03 +0100

    array range used instead of date interval. THis after pair programming with andrew.

--- LeaveCalculator.php
+++ LeaveCalculator.php
@@ -7,62 +7,65 @@
  */
 
 
+
+
+
 class LeaveCalculator {
 
+
+ function __construct($params){
+        echo $params;
+
+
+    }
 
 public function getWeekNumber($datetime){
 
 
-    $year = new DateTime($datetime);
-    $year = $year->format('Y');
+    $formatDate = new DateTime($datetime);
+//    $year = $formatDate->format('Y');
+//
+//    $nextYear = $year +1;
+//
+//    $begin = new DateTime( $year.'-04-01' );
+//    $end = new DateTime( $nextYear.'-03-31' );
+//
+//    $interval = DateInterval::createFromDateString('1 week');
+//    $period = new DatePeriod($begin, $interval, $end);
+//    $weekNumberArray = array();
 
-    $nextYear = $year +1;
+//    foreach ( $period as $key=>$dt ) {
+//    array_push($weekNumberArray,$dt->format("W"));
+//
+//    }
 
-    $begin = new DateTime( $year.'-04-01' );
-    $end = new DateTime( $nextYear.'-03-31' );
+   $weekNumberArray = array_merge(range(40,52), range(1,39));
+    
+        //var_dump($weekNumberArray);
 
-    $interval = DateInterval::createFromDateString('1 week');
-    $period = new DatePeriod($begin, $interval, $end);
-    $weekNumberArray = array();
+    $week = intval($formatDate->format("W"));
+    $returnKey = $weekNumberArray[$week-1];
 
-    foreach ( $period as $key=>$dt ) {
-    array_push($weekNumberArray,$dt->format("W"));
-
-    }
-
-   // var_dump($weekNumberArray);
-
-      // echo " ".$key;
-    $date = new DateTime($datetime);
-    $week = $date->format("W");
-
-    $returnKey = array_search($week,$weekNumberArray);
-
-       // echo $returnKey+1;
-
-    return $returnKey;
-    //  return $key +1;
-
-
+        echo $returnKey;
+       // return $returnKey;
 }
 
     public function daysHoliday($weekNumber,$totalHoliday){
-        $seq = array(0.5,1,1.5,2,2.5,3,3.5,4,4.5,5,5.5,6,6.6,7,7.5,8,8.5,9,9.5,10,10.5,11,11,11.5,12,12.5,13,13.5,14,14.5,15,15.5,16,16.5,17,17.5,18,18.5,19,19.5,20,20.5,21.5,22,22.5,23,23,23.5,24,24.5,25);
-   //   $seqSort =  $seq[$weekNumber-1];
-     //  $daysHoliday = 25 - $seqSort;
-         // echo $daysHoliday;
-           echo  $totalHoliday- $seq[$weekNumber];
-            return $totalHoliday- $seq[$weekNumber];
+        $seq = [0.5,1,1.5,2,2.5,3,3.5,4,4.5,5,5.5,6,6.6,7,7.5,8,8.5,9,9.5,10,10.5,11,11,11.5,12,12.5,13,13.5,14,14.5,15,15.5,16,16.5,17,17.5,18,18.5,19,19.5,20,20.5,21,21.5,22,22.5,23,23,23.5,24,24.5,25];
+       // echo  $totalHoliday- $seq[$weekNumber];
+       // echo  $seq[$weekNumber];
+        return $totalHoliday- $seq[$weekNumber];
     }
 
 }
+$dfg =2016/04/01;
+
+$date = $_GET['input'];
 
 
+$lC = new LeaveCalculator($params);
 
-$lC = new LeaveCalculator();
+$date = "2016/01/01";
 
-
-$date = new DateTime();
-
-$weekNumberKey = $lC->getWeekNumber($datetime = '2016/04/03');
+$weekNumberKey = $lC->getWeekNumber($date);
 $lC->daysHoliday($weekNumberKey,25);
